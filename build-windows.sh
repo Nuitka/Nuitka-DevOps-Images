592ca8ff309fe2b2dc09d640d9cfc71fb2251a1c
Maxwell Dreytser <admin@mdtech.us>
2024-01-13 15:50:52 -0500

    Don't create a new identity every time.

--- build-windows.sh
+++ build-windows.sh
@@ -12,34 +12,13 @@ sigName=DevOps_images
 imageDefName=NuitkaWindowsDevOps
 runOutputName=NuitkaWindowsDevOps
 
-# Create user-assigned identity for VM Image Builder to access the storage account where the script is stored
-identityName=aibBuiUserId$(date +'%s')
-az identity create -g $sigResourceGroup -n $identityName
+identityName=aibBuiUserId
 
 # Get the identity ID
 imgBuilderCliId=$(az identity show -g $sigResourceGroup -n $identityName --query clientId -o tsv)
 
 # Get the user identity URI that's needed for the template
 imgBuilderId=/subscriptions/$subscriptionID/resourcegroups/$sigResourceGroup/providers/Microsoft.ManagedIdentity/userAssignedIdentities/$identityName
-
-# Download an Azure role-definition template, and update the template with the parameters that were specified earlier
-curl https://raw.githubusercontent.com/Azure/azvmimagebuilder/master/solutions/12_Creating_AIB_Security_Roles/aibRoleImageCreation.json -o aibRoleImageCreation.json
-
-imageRoleDefName="Azure Image Builder Image Def"$(date +'%s')
-
-# Update the definition
-sed -i -e "s/<subscriptionID>/$subscriptionID/g" aibRoleImageCreation.json
-sed -i -e "s/<rgName>/$sigResourceGroup/g" aibRoleImageCreation.json
-sed -i -e "s/Azure Image Builder Service Image Creation Role/$imageRoleDefName/g" aibRoleImageCreation.json
-
-# Create role definitions
-az role definition create --role-definition ./aibRoleImageCreation.json
-
-# Grant a role definition to the user-assigned identity
-az role assignment create \
-    --assignee $imgBuilderCliId \
-    --role "$imageRoleDefName" \
-    --scope /subscriptions/$subscriptionID/resourceGroups/$sigResourceGroup
 
 cp NuitkaDevOpsWindowsImage.json NuitkaDevOpsWindowsImage_tmp.json
 
@@ -72,12 +51,3 @@ az resource delete \
     --resource-group $sigResourceGroup \
     --resource-type Microsoft.VirtualMachineImages/imageTemplates \
     -n $imageName
-
-az role assignment delete \
-    --assignee $imgBuilderCliId \
-    --role "$imageRoleDefName" \
-    --scope /subscriptions/$subscriptionID/resourceGroups/$sigResourceGroup
-
-az role definition delete --name "$imageRoleDefName"
-
-az identity delete --ids $imgBuilderId
